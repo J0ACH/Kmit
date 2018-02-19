61cf5bfd1992158eafc19a6a69fb46945575eb83
Jachym Pesek <jachym.pesek@gmail.com>
2018-02-19 19:56:29 +0100

    jui linked

--- source/main.cpp
+++ source/main.cpp
@@ -2,18 +2,28 @@
 
 #include <QApplication>
 #include <QWidget>
+
+#include <core.h>
+#include <screens.h>
 #include <scserver.h>
 
+using namespace Jui;
 using namespace SC;
 
 int main(int argc, char *argv[]) {
 
 	QApplication app(argc, argv);
 	
+	/*
 	QWidget *win = new QWidget();
 	win->setGeometry(100, 100, 800, 700);
 	win->show();
+	*/
 
+	
+	Win *win = new Win(100,100,800,700);
+	win->name_("Kmit");
+		
 	SC::ScServer server(win);	
 	server.setPath("C:/Program Files/SuperCollider-3.9.0");
 
